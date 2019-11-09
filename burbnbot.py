6bf388199cc558d6ffde6634205ed1892b9e413e
harrypython <harrypython@protonmail.com>
2019-11-08 16:36:15 -0800

    like by hashtag working clarifai working with photo and video

--- burbnbot.py
+++ burbnbot.py
@@ -1,6 +1,5 @@
 from BurbnBot.BurbnBot import BurbnBot
 
-# https://www.instagram.com/p/B4fgT13nYkZ/?igshid=bv81bcl5jz0n
-burbn = BurbnBot(configfile="config/credentials.json")
+burbn = BurbnBot()
 
-burbn.hashtag_interact(hashtag="chicagomodel", aba="top", stories=False)
+burbn.hashtag_interact(hashtag="bikinibabe", aba="recents", stories=False)
