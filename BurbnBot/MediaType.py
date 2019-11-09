875b1675ce581aedf3b294e9f330d621c781fd8e
harrypython <harrypython@protonmail.com>
2019-11-08 23:23:30 -0800

    MediaType Instabot to do the dirt jobs Appium/driver only on actions Unfollow and unfollow_non_followers logger is better than logging

---
+++ BurbnBot/MediaType.py
@@ -0,0 +1,4 @@
+class MediaType(object):
+    PHOTO = 1
+    VIDEO = 2
+    CAROUSEL = 8
--- burbnbot.py
+++ burbnbot.py
@@ -2,4 +2,4 @@ from BurbnBot.BurbnBot import BurbnBot
 
 burbn = BurbnBot()
 
-burbn.hashtag_interact(hashtag="bikinibabe", aba="recents", stories=False)
+burbn.unfollow(user="kctvo")
